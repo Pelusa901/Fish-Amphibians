14e44973f7504f30c4ad0a2ca203acc71be2e19c
Pelusa901 <alicia.saunders@questu.ca>
2016-12-19 11:21:07 -0800

    mon

--- amps.R
+++ amps.R
@@ -10,9 +10,24 @@ length(unique(amp$Site))
 #Find the blank one
 (bad <- amp[amp$Site == "", ]) #1114
 #Great, looks like we can delete it because it was just a copying error
-amps <- amp[1:1113, ]
+amp1 <- amp[1:1113, ]
+str(amp1)
+?drop
+amps <- amp[-(nrow(amp)), ]
 str(amps)
+unique(amps$Site)
 length(unique(amps$Site)) # 6!!
+
+remove(amps)
+#Git rid of some colums
+colnames(amps)
+amps <- amp[1:1113, c(1, 17)]
+str(amps)
+
+(s1 <- amps[amps$Sites == "LS4", ])
+(s2 <- amps[amps$Sites == "Wood Lake - LS3C", ])
+#count number of species in each site
+s1 <- 
 
 # Rename the sites for simplicity
 amps$sites
